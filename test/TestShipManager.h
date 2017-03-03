428c0ae6912b33eea962b289dc930efbcb77b52d
Alzater <alzater@mail.ru>
2017-03-03 15:20:02 +0300

    tests ShipManager for Left and Right movements

--- test/TestShipManager.h
+++ test/TestShipManager.h
@@ -41,6 +41,18 @@ public:
         TS_ASSERT_EQUALS( newPosition.isVertical, true);
     }
 
+    void testMoveUpBarrier()
+    {
+        Field field(4, 4);
+        field.setBarrier(1, 0);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {1, 2, true, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::UP);
+
+        TS_ASSERT_EQUALS( newPosition.isLegal, false);
+    }
+
     void testMoveUpBorder()
     {
         Field field(4, 4);
@@ -77,6 +89,19 @@ public:
         TS_ASSERT_EQUALS( newPosition.isVertical, true);
     }
 
+    void testMoveDownBarrier()
+    {
+        Field field(4, 4);
+        field.setBarrier(1, 3);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {1, 1, true, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::DOWN);
+
+        TS_ASSERT_EQUALS( newPosition.isLegal, false);
+    }
+
+
     void testMoveDownBorder()
     {
         Field field(4, 4);
@@ -95,6 +120,102 @@ public:
         ShipManager::Position startPosition = {2, 1, false, true};
 
         auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::DOWN);
+
+        TS_ASSERT_EQUALS( newPosition.isLegal, false);
+    }
+
+    void testMoveRight()
+    {
+        Field field(4, 4);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {1, 1, false, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::RIGHT);
+
+        TS_ASSERT_EQUALS( newPosition.x, 2);
+        TS_ASSERT_EQUALS( newPosition.y, 1);
+        TS_ASSERT_EQUALS( newPosition.isLegal, true);
+        TS_ASSERT_EQUALS( newPosition.isVertical, false);
+    }
+
+    void testMoveRightBarrier()
+    {
+        Field field(4, 4);
+        field.setBarrier(3, 1);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {1, 1, false, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::RIGHT);
+
+        TS_ASSERT_EQUALS( newPosition.isLegal, false);
+    }
+
+    void testMoveRightBorder()
+    {
+        Field field(4, 4);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {2, 2, false, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::RIGHT);
+
+        TS_ASSERT_EQUALS( newPosition.isLegal, false);
+    }
+
+    void testMoveRightVertical()
+    {
+        Field field(4, 4);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {2, 1, true, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::RIGHT);
+
+        TS_ASSERT_EQUALS( newPosition.isLegal, false);
+    }
+
+    void testMoveLeft()
+    {
+        Field field(4, 4);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {2, 1, false, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::LEFT);
+
+        TS_ASSERT_EQUALS( newPosition.x, 1);
+        TS_ASSERT_EQUALS( newPosition.y, 1);
+        TS_ASSERT_EQUALS( newPosition.isVertical, false);
+        TS_ASSERT_EQUALS( newPosition.isLegal, true);
+    }
+
+    void testMoveLeftBarrier()
+    {
+        Field field(4, 4);
+        field.setBarrier(0, 1);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {2, 1, false, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::LEFT);
+
+        TS_ASSERT_EQUALS( newPosition.isLegal, false);
+    }
+
+    void testMoveLeftBorder()
+    {
+        Field field(4, 4);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {1, 2, false, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::LEFT);
+
+        TS_ASSERT_EQUALS( newPosition.isLegal, false);
+    }
+
+    void testMoveLeftVertical()
+    {
+        Field field(4, 4);
+        ShipManager shm(field);
+        ShipManager::Position startPosition = {2, 1, true, true};
+
+        auto newPosition = shm.getNewPosition(startPosition, ShipManager::MOVEMENT::LEFT);
 
         TS_ASSERT_EQUALS( newPosition.isLegal, false);
     }
